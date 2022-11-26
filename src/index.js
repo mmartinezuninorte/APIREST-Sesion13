c6b1b2c44a062ff60e6a57dd42922bc57573ee9b
mmartinezuninorte <mmartinezuninorte@gmail.com>
2022-11-26 09:49:36 -0500

    Conexion realizada base de datos

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-// const express = require('express')
-
 import express from 'express'
+import mongoose from 'mongoose'
 
 const app = express()
 
 app.set('port', 3000)
 
 app.listen(app.get('port'),()=>{
-    console.log("Servidor en puerto ", app.get('port'))
+    console.log("Servidor levantado en  puerto ", app.get('port'))
 })
 
 app.get('/',(request, response)=>{
@@ -16,4 +15,9 @@ app.get('/',(request, response)=>{
 
 app.post('/',(request, response)=>{
     response.json({message: "Estas accediendo mediante una peticion POST"})
-})
+});
+
+(async()=>{
+    const db = await mongoose.connect('mongodb+srv://admin:Wi6G96YmCA60fc9m@cluster0.gfhuiwi.mongodb.net/?retryWrites=true&w=majority')
+    console.log('Conectado a la base de datos: ', db.connection.name)
+})()
